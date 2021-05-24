f8b8cf5184e0ee0ec6460887ed4c3589673a5070
dlaumer <daniel.laumer@gmail.com>
2021-05-24 08:53:39 +0200

    Video per youtube

--- app/js/Video.js
+++ app/js/Video.js
@@ -27,16 +27,21 @@ define([
         init: function () {
 
             //domCtr.create("video", { id: "instructions", src: "images/instructions.mov"}, this.containerVideo);
-            this.container1 = domCtr.create("div", { id: "container1", className: "containerTypeInfo" }, this.containerVideo);
+            this.container1 = domCtr.create("div", { id: "containerVideo" }, this.containerVideo);
+            /*
             this.container1.innerHTML = 
-
             `<video width=100% height="auto" id="instructions" autoplay controls>
                 <source src="images/instructions.mov" type="video/mp4">
             Your browser does not support the video tag.
             </video>`
+            */
+            this.container1.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/Hw5gPbqw1ts?rel=0&autoplay=1&mute=1" 
+            title="UrbanMobility" frameborder="0" allow="accelerometer; autoplay; clipboard-write; 
+            encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            `
             this.finishButton = domCtr.create("div", { id: "finishButton", className: "task_button", innerHTML: "Yes I watched it!" }, this.containerQuest);
             this.settings.dev ? "" : this.finishButton.style.pointerEvents = 'none';
-
+            
             
             this.clickHandler();
         
@@ -49,12 +54,20 @@ define([
                 this.settings.home.returnToHome({watched: "yes"});
             }.bind(this));
 
+            this.timer = setTimeout(() => {
+                this.finishButton.style.pointerEvents = 'auto';
+            this.finishButton.className = "task_button active";
+            },128000);
+
+            
+            
+/*
             var that = this;
             dom.byId('instructions').addEventListener('ended',myHandler,false);
                 function myHandler(e) {
                     that.finishButton.style.pointerEvents = 'auto';
                     that.finishButton.className = "task_button active" 
-                }
+            }*/
         },
 
 
